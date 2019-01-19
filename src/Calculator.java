08be382a4ad77d15fb345bbd74b0ab211bc09ee3
prljaca1 <prljacaaysenur@gmail.com>
2019-01-19 12:59:40 -0500

    UPTADED

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,11 @@
 public class Calculator {
     public static double add(double num1,double num2){
+
         return num1+num2;
+    }
+
+    public static double substract(double num1,double num2){
+        double result=num1-num2;
+        return result;
     }
 }
